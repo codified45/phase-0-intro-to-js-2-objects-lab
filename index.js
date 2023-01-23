d25413b7e09ed2e6f0267cd705bee37f9e97fbd3
Jack Lach <jack45k@gmail.com>
2023-01-23 08:39:30 -0500

    objects lab all 7 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "John",
+    streetAddress: "55th Street",
+};
+
+const updateEmployeeWithKeyAndValue = function(employee, key, value){
+    const updatedEmployee = {...employee}
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = function(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+const deleteFromEmployeeByKey = function(employee, key){
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key]; 
+    return updatedEmployee;
+}
+
+const destructivelyDeleteFromEmployeeByKey = function(employee, key){
+    delete employee[key];
+    return employee;
+}
